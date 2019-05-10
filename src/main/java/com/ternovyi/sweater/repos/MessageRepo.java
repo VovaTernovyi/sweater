4b935cd1b20df35e6d08d951219dd5322e1158d2
VovaTernyi <vovaternovy@gmail.com>
2019-05-10 16:06:23 +0300

    JPA init.

---
+++ src/main/java/com/ternovyi/sweater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.ternovyi.sweater.repos;
+
+import com.ternovyi.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
